da3fc7b190a2eec828f8439580e6e23940c6fb3d
M7moud S3d <62154958+mahmoudmohamedsaad@users.noreply.github.com>
2022-09-16 08:58:51 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const newYears = "1 Dec 2023";
+
+function countdown(){
+    const newYearDate = new Date(newYears);
+    const currentDate = new Date();
+    const totalSeconds = (newYearDate - currentDate) /1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+countdown();
+setInterval(countdown,1000);
+
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
